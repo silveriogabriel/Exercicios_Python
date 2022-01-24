912b684887f3d71796bca688a83d907968ace9b0
Gabriel Silvério <gabriel.v.silverio@gmail.com>
2022-01-24 09:54:39 -0300

    Exercicios feitos
    Atualizando lista de exercicios com mais 3 exercicios resolvidos

--- Exercicios/Ex005.py
+++ Exercicios/Ex005.py
@@ -1,1 +1,4 @@
 '''Faça um programa que leia um valor pelo teclado e mostre na tela seu sucessor e seu antesessor'''
+
+n1 = int(input('Digite um numero inteiro: '))
+print(f'O valor digitado foi {n1} seu sucessor é {n1 + 1} e seu antecessor é {n1 - 1}')
--- Exercicios/Ex006.py
+++ Exercicios/Ex006.py
@@ -1,1 +1,5 @@
 '''Crie um algoritimo que leia um numero e mostre na tela seu dobro, triplo, e raiz quadrada'''
+
+valor = int(input('Digite um valor inteiro: '))
+
+print(f'O valor digitado foi {valor} seu dobro é {valor*2} o triplo é {valor * 3} e a raiz quadrada é {valor ** 2/1}.')
